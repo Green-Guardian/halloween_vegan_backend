8276de2d77c409e4af8381e3002b2da075985767
seal <osipov.pv1000@gmail.com>
2023-12-26 00:19:15 +0300

    fix: paths

--- app/halloween_vegan_backend/settings.py
+++ app/halloween_vegan_backend/settings.py
@@ -55,7 +55,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'halloween_vegan_backend.urls'
+ROOT_URLCONF = 'app.halloween_vegan_backend.urls'
 
 TEMPLATES = [
     {
@@ -73,7 +73,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'halloween_vegan_backend.wsgi.application'
+WSGI_APPLICATION = 'app.halloween_vegan_backend.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
