9ead58496abe508212a53f12ae98d66380dceec9
seal <osipov.pv1000@gmail.com>
2023-12-25 23:51:58 +0300

    fix: path

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -6,4 +6,4 @@ echo "[INFO] Running django tests: $ python ./app/manage.py test"
 python app/manage.py test
 
 echo "[INFO] Launching the django server: $ daphne -b 0.0.0.0 -p 8000 app/halloween_vegan_backend.asgi:application"
-daphne -b 0.0.0.0 -p 8000 app/halloween_vegan_backend.asgi:application
+daphne -b 0.0.0.0 -p 8000 app.halloween_vegan_backend.asgi:application
