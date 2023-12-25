aee5f5371a8057b3d902b1c6d280d1413262dd78
seal <osipov.pv1000@gmail.com>
2023-12-25 23:45:43 +0300

    feat: added server "Daphne"

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -5,5 +5,5 @@ set -e
 echo "[INFO] Running django tests: $ python ./app/manage.py test"
 python app/manage.py test
 
-echo "[INFO] Launching the django TEST! server: $ python app/manage.py runserver 0.0.0.0:8000"
-python app/manage.py runserver 0.0.0.0:8000
+echo "[INFO] Launching the django server: $ daphne -b 0.0.0.0 -p 8000 app/halloween_vegan_backend.asgi:application"
+daphne -b 0.0.0.0 -p 8000 app/halloween_vegan_backend.asgi:application
