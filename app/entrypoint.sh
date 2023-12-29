541cc141a2deb030f5314442b1e79127154fdf9c
seal <osipov.pv1000@gmail.com>
2023-12-29 22:47:26 +0300

    fix: OAuth

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -8,5 +8,9 @@ python app/manage.py collectstatic --noinput
 #echo "[INFO] Running django tests: $ python ./app/manage.py test"
 #python app/manage.py test
 
+#echo "[INFO] Running django oauth2_provider migration: $ app/manage.py migrate oauth2_provider"
+python app/manage.py migrate oauth2_provider
+
+
 echo "[INFO] Launching the django server: $ daphne -b 0.0.0.0 -p 8000 app.halloween_vegan_backend.asgi:application"
 daphne -b 0.0.0.0 -p 8000 app.halloween_vegan_backend.asgi:application
