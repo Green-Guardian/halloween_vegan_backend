02d2ed9b47a0a5ee5274c70fa5d62c9afd4911db
seal <osipov.pv1000@gmail.com>
2024-02-18 20:47:23 +0300

    test:

--- app/chat/routing.py
+++ app/chat/routing.py
@@ -2,5 +2,5 @@ from django.urls import re_path
 from app.chat import consumers
 
 websocket_urlpatterns = [
-    re_path(r'ws/app/chat/$', consumers.ChatConsumer.as_asgi()),
+    re_path(r'^ws/app/chat/$', consumers.ChatConsumer.as_asgi()),
 ]
