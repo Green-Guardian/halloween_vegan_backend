65f7028a658bb484722d1ec1f2af00ab820f7d2a
seal <osipov.pv1000@gmail.com>
2024-02-21 00:21:18 +0300

    feat: added style for chat

--- app/chat/consumers.py
+++ app/chat/consumers.py
@@ -12,22 +12,28 @@ class ChatConsumer(AsyncWebsocketConsumer):
 
     async def connect(self):
         self.logger.debug('WebSocket connection established')
+
         await self.channel_layer.group_add(
             "hw_admin_chat",
             self.channel_name
         )
         await self.accept()
 
+        # Получение истории сообщений
         messages = await self.get_chat_messages()
         for message, username in messages:
+            # Проверка, отправлено ли сообщение текущим пользователем
+            is_sent_by_user = username == self.scope["user"].username
             await self.send(text_data=json.dumps({
+                'type': 'chat_history',
                 'message': message,
-                'user': username
+                'user': username,
+                "status": "sent" if is_sent_by_user else "received",
             }))
 
     @database_sync_to_async
     def get_chat_messages(self):
-        messages = ChatMessage.objects.all().order_by('-created_at')[:20]
+        messages = ChatMessage.objects.all().order_by('-created_at')[:100]
         messages_list = [(message.message, message.user.username) for message in messages]
         messages_list.reverse()
         return messages_list
@@ -42,6 +48,7 @@ class ChatConsumer(AsyncWebsocketConsumer):
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
+        username = self.scope["user"].username
 
         await self.create_chat_message(user=self.scope['user'], message=message)
 
@@ -49,7 +56,8 @@ class ChatConsumer(AsyncWebsocketConsumer):
             "hw_admin_chat",
             {
                 'type': 'chat_message',
-                'message': message
+                'message': message,
+                'username': username,
             }
         )
 
@@ -58,8 +66,12 @@ class ChatConsumer(AsyncWebsocketConsumer):
         return ChatMessage.objects.create(user=user, message=message)
 
     async def chat_message(self, event):
-        message = event['message']
+        # Определяем, отправлено ли сообщение текущим пользователем
+        is_sent_by_user = self.scope["user"].username == event.get("username")
 
         await self.send(text_data=json.dumps({
-            'message': message
+            'message': event['message'],
+            'user': event.get("username"),
+            'type': 'chat_message',
+            'status': 'sent' if is_sent_by_user else 'received'
         }))
---
+++ app/chat/static/app/chat.js
@@ -0,0 +1,70 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var chatSocket = new WebSocket(
+        (window.location.protocol === "https:" ? "wss://" : "ws://") + window.location.host + '/ws/app/chat/'
+    );
+
+    chatSocket.onmessage = function (e) {
+        var data = JSON.parse(e.data);
+        if (data.type === "chat_message") {
+            addMessage(data.message, data.user, data.status); // 'sent' или 'received'
+        } else if (data.type === "chat_history") {
+            addMessage(data.message, data.user, data.status);
+        }
+    };
+
+
+    var sendMessage = function () {
+        var messageInputDom = document.querySelector('.chat__conversation-panel__input');
+        var message = messageInputDom.value;
+        chatSocket.send(JSON.stringify({
+            'message': message
+        }));
+        messageInputDom.value = '';
+    };
+
+    document.querySelector('.send-message-button').addEventListener('click', function (e) {
+        sendMessage();
+    });
+
+    document.querySelector('.chat__conversation-panel__input').addEventListener('keypress', function (e) {
+        if (e.key === 'Enter' && !e.shiftKey) {
+            e.preventDefault();
+            sendMessage();
+        }
+    });
+
+    // Функция для добавления сообщения в .chat__conversation-board
+    function addMessage(message, user, status) {
+        var chatLog = document.querySelector('.chat__conversation-board');
+
+        var messageContainer = document.createElement('div');
+        messageContainer.classList.add('chat__conversation-board__message-container');
+        if (status === 'sent') {
+            messageContainer.classList.add('reversed');
+        }
+
+        var avatarContainer = document.createElement('div');
+        avatarContainer.classList.add('chat__conversation-board__message__person');
+
+        var avatarImg = document.createElement('img');
+        // Используйте статический URL для изображения аватара пользователя или дефолтный аватар
+        avatarImg.src = "https://seal-pavel.website/static/app/favicon.ico"; // Пример URL к аватарке
+        avatarImg.classList.add('chat__conversation-board__message__person__avatar');
+        avatarContainer.appendChild(avatarImg);
+
+        var messageTextContainer = document.createElement('div');
+        messageTextContainer.classList.add('chat__conversation-board__message__context');
+
+        var messageBubble = document.createElement('div');
+        messageBubble.classList.add('chat__conversation-board__message__bubble');
+        messageBubble.innerHTML = `<span>${message}</span>`;
+        messageTextContainer.appendChild(messageBubble);
+
+        messageContainer.appendChild(avatarContainer);
+        messageContainer.appendChild(messageTextContainer);
+
+        chatLog.appendChild(messageContainer);
+        chatLog.scrollTop = chatLog.scrollHeight;
+    }
+
+});
